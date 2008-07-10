291a04d2615404bd369fe0906256705b577f896d
drmoriarty.0 <DrMoriarty@users.noreply.github.com>
2008-07-10 08:27:14 +0000

    work

---
+++ cuberok_ru.ts
@@ -0,0 +1,545 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="ru">
+<context>
+    <name>AboutDialogClass</name>
+    <message>
+        <location filename="src/aboutdialog.ui" line="19"/>
+        <source>AboutDialog</source>
+        <translation>О Программе</translation>
+    </message>
+    <message>
+        <location filename="src/aboutdialog.ui" line="49"/>
+        <source>Cuberok</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/aboutdialog.ui" line="76"/>
+        <source>v 0.0.1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/aboutdialog.ui" line="86"/>
+        <source>&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;MS Shell Dlg 2&apos;; font-size:8.25pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:8pt;&quot;&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/aboutdialog.ui" line="96"/>
+        <source>Close</source>
+        <translation>Закрыть</translation>
+    </message>
+</context>
+<context>
+    <name>CollectionModel</name>
+    <message>
+        <location filename="src/collectionview.cpp" line="239"/>
+        <source>artists</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/collectionview.cpp" line="246"/>
+        <source>albums</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/collectionview.cpp" line="253"/>
+        <source>genres</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/collectionview.cpp" line="287"/>
+        <source>songs</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CollectionView</name>
+    <message>
+        <location filename="src/collectionview.cpp" line="456"/>
+        <source>New Album</source>
+        <translation>Новый Альбом</translation>
+    </message>
+    <message>
+        <location filename="src/collectionview.cpp" line="459"/>
+        <source>New Artist</source>
+        <translation>Новый Исполнитель</translation>
+    </message>
+    <message>
+        <location filename="src/collectionview.cpp" line="462"/>
+        <source>New Genre</source>
+        <translation>Новый Жанр</translation>
+    </message>
+</context>
+<context>
+    <name>CuberokClass</name>
+    <message>
+        <location filename="src/cuberok.ui" line="13"/>
+        <source>Cuberok</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="361"/>
+        <source>Collection</source>
+        <translation>Коллекция</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="163"/>
+        <source>TextLabel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="176"/>
+        <source>...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="198"/>
+        <source>Enter search substring</source>
+        <translation>Введите строку поиска</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="234"/>
+        <source>Files</source>
+        <translation>Файлы</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="543"/>
+        <source>File</source>
+        <translation>Файл</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="293"/>
+        <source>Help</source>
+        <translation>Помощь</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="299"/>
+        <source>Control</source>
+        <translation>Управление</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="313"/>
+        <source>View</source>
+        <translation>Вид</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="317"/>
+        <source>Columns</source>
+        <translation>Столбцы</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="348"/>
+        <source>List</source>
+        <translation>Список</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="367"/>
+        <source>toolBar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="395"/>
+        <source>About</source>
+        <translation>О Программе</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="398"/>
+        <source>About dialog</source>
+        <translation>Информация о программе</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="401"/>
+        <source>Alt+F1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="415"/>
+        <source>Break</source>
+        <translation>Отмена</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="423"/>
+        <source>Previous</source>
+        <translation>Предыдущий</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="426"/>
+        <source>&lt;&lt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="432"/>
+        <source>Previous track</source>
+        <translation>Предыдущий трек</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="440"/>
+        <source>Next</source>
+        <translation>Следующий</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="443"/>
+        <source>&gt;&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="449"/>
+        <source>Next track</source>
+        <translation>Следующий трек</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="469"/>
+        <source>Pause</source>
+        <translation>Пауза</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="463"/>
+        <source>||</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="477"/>
+        <source>Play</source>
+        <translation>Играть</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="480"/>
+        <source>&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="486"/>
+        <source>Play track</source>
+        <translation>Играть трек</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="494"/>
+        <source>Shuffle</source>
+        <translation>Тасовать</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="497"/>
+        <source>Shuffle mode</source>
+        <translation>Режим перетасовки</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="505"/>
+        <source>Repeat</source>
+        <translation>Повторять</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="508"/>
+        <source>Repeat mode</source>
+        <translation>Режим повтора</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="513"/>
+        <source>Exit</source>
+        <translation>Выход</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="516"/>
+        <source>Close the program</source>
+        <translation>Закрыть программу</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="527"/>
+        <source>Clear playlist</source>
+        <translation>Очистить список воспроизведения</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="524"/>
+        <source>Clear</source>
+        <translation>Очистить</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="532"/>
+        <source>Queue as Next</source>
+        <translation>Поставить в очередь</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="554"/>
+        <source>Track</source>
+        <translation>Трек</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="565"/>
+        <source>Title</source>
+        <translation>Название</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="678"/>
+        <source>Artist</source>
+        <translation>Исполнитель</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="683"/>
+        <source>Album</source>
+        <translation>Альбом</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="688"/>
+        <source>Genre</source>
+        <translation>Жанр</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="609"/>
+        <source>Year</source>
+        <translation>Год</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="620"/>
+        <source>Comment</source>
+        <translation>Комментарий</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="631"/>
+        <source>Correct cp1251 in ID3v1</source>
+        <translation>Исправлять кодировку cp1251 в ID3v1</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="642"/>
+        <source>Length</source>
+        <translation>Длительность</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="653"/>
+        <source>Edit Tag</source>
+        <translation>Редактировать тэги</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="650"/>
+        <source>Tag</source>
+        <translation>Тэги</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="658"/>
+        <source>New playlist</source>
+        <translation>Новый список воспроизведения</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="663"/>
+        <source>Close playlist</source>
+        <translation>Закрыть список воспроизведения</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="668"/>
+        <source>Remove song</source>
+        <translation>Удалить песню</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="673"/>
+        <source>Look and Feel</source>
+        <translation>Внешний вид</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="693"/>
+        <source>Song</source>
+        <translation>Песня</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="698"/>
+        <source>Add Item</source>
+        <translation>Добавить элемент</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="703"/>
+        <source>Remove Item</source>
+        <translation>Удалить элемент</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="717"/>
+        <source>Stop</source>
+        <translation>Стоп</translation>
+    </message>
+    <message>
+        <location filename="src/cuberok.ui" line="714"/>
+        <source>[]</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LookAndFeelClass</name>
+    <message>
+        <location filename="src/lookandfeel.ui" line="13"/>
+        <source>LookAndFeel</source>
+        <translation>Внешний вид</translation>
+    </message>
+    <message>
+        <location filename="src/lookandfeel.ui" line="42"/>
+        <source>Colors</source>
+        <translation>Цвета</translation>
+    </message>
+    <message>
+        <location filename="src/lookandfeel.ui" line="32"/>
+        <source>Style</source>
+        <translation>Стиль</translation>
+    </message>
+    <message>
+        <location filename="src/lookandfeel.ui" line="48"/>
+        <source>Group</source>
+        <translation>Группа</translation>
+    </message>
+    <message>
+        <location filename="src/lookandfeel.ui" line="58"/>
+        <source>Role</source>
+        <translation>Роль</translation>
+    </message>
+    <message>
+        <location filename="src/lookandfeel.ui" line="68"/>
+        <source>Brush</source>
+        <translation>Кисть</translation>
+    </message>
+    <message>
+        <location filename="src/lookandfeel.ui" line="78"/>
+        <source>Color</source>
+        <translation>Цвет</translation>
+    </message>
+    <message>
+        <location filename="src/lookandfeel.ui" line="88"/>
+        <source>Quick colors</source>
+        <translation>Быстрые цвета</translation>
+    </message>
+    <message>
+        <location filename="src/lookandfeel.ui" line="96"/>
+        <source>Fonts</source>
+        <translation>Шрифты</translation>
+    </message>
+    <message>
+        <location filename="src/lookandfeel.ui" line="125"/>
+        <source>PushButton</source>
+        <translation>Кнопка</translation>
+    </message>
+    <message>
+        <location filename="src/lookandfeel.ui" line="149"/>
+        <source>Line Edit</source>
+        <translation>Строка теста</translation>
+    </message>
+    <message>
+        <location filename="src/lookandfeel.ui" line="182"/>
+        <source>GroupBox</source>
+        <translation>Группа</translation>
+    </message>
+    <message>
+        <location filename="src/lookandfeel.ui" line="172"/>
+        <source>RadioButton</source>
+        <translation>Радио-кнопка</translation>
+    </message>
+    <message>
+        <location filename="src/lookandfeel.ui" line="205"/>
+        <source>CheckBox</source>
+        <translation>Кнопка-флаг</translation>
+    </message>
+    <message>
+        <location filename="src/lookandfeel.ui" line="252"/>
+        <source>Cancel</source>
+        <translation>Отмена</translation>
+    </message>
+    <message>
+        <location filename="src/lookandfeel.ui" line="259"/>
+        <source>Apply</source>
+        <translation>Применить</translation>
+    </message>
+    <message>
+        <location filename="src/lookandfeel.ui" line="266"/>
+        <source>Ok</source>
+        <translation>Да</translation>
+    </message>
+</context>
+<context>
+    <name>PlaylistModel</name>
+    <message>
+        <location filename="src/playlistmodel.cpp" line="97"/>
+        <source>Error</source>
+        <translation>Ошибка</translation>
+    </message>
+    <message>
+        <location filename="src/playlistmodel.cpp" line="70"/>
+        <source></source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TagEditorClass</name>
+    <message>
+        <location filename="src/tageditor.ui" line="13"/>
+        <source>TagEditor</source>
+        <translation>Редактор тегов</translation>
+    </message>
+    <message>
+        <location filename="src/tageditor.ui" line="22"/>
+        <source>File</source>
+        <translation>Файл</translation>
+    </message>
+    <message>
+        <location filename="src/tageditor.ui" line="32"/>
+        <source>Cancel</source>
+        <translation>Отмена</translation>
+    </message>
+    <message>
+        <location filename="src/tageditor.ui" line="39"/>
+        <source>Save</source>
+        <translation>Записать</translation>
+    </message>
+    <message>
+        <location filename="src/tageditor.ui" line="52"/>
+        <source>Correct cp1251</source>
+        <translation>Исправить cp1251</translation>
+    </message>
+    <message>
+        <location filename="src/tageditor.ui" line="59"/>
+        <source>Correct 7bit cp1251</source>
+        <translation>Исправить 7 битный cp1251</translation>
+    </message>
+    <message>
+        <location filename="src/tageditor.ui" line="75"/>
+        <source>Title</source>
+        <translation>Название</translation>
+    </message>
+    <message>
+        <location filename="src/tageditor.ui" line="82"/>
+        <source>Artist</source>
+        <translation>Исполнитель</translation>
+    </message>
+    <message>
+        <location filename="src/tageditor.ui" line="89"/>
+        <source>Album</source>
+        <translation>Альбом</translation>
+    </message>
+    <message>
+        <location filename="src/tageditor.ui" line="99"/>
+        <source>Comment</source>
+        <translation>Комментарий</translation>
+    </message>
+    <message>
+        <location filename="src/tageditor.ui" line="113"/>
+        <source>Genre</source>
+        <translation>Жанр</translation>
+    </message>
+    <message>
+        <location filename="src/tageditor.ui" line="134"/>
+        <source>Track</source>
+        <translation>Трек</translation>
+    </message>
+    <message>
+        <location filename="src/tageditor.ui" line="141"/>
+        <source>Year</source>
+        <translation>Год</translation>
+    </message>
+    <message>
+        <location filename="src/tageditor.ui" line="151"/>
+        <source>Rating</source>
+        <translation>Рэйтинг</translation>
+    </message>
+</context>
+<context>
+    <name>collectionviewClass</name>
+    <message>
+        <location filename="src/collectionview.ui" line="13"/>
+        <source>collectionview</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
--- win_build.bat
+++ win_build.bat
@@ -1,2 +1,4 @@
+C:\Programs\Qt\4.3.1\bin\lupdate.exe Cuberok.pro
+C:\Programs\Qt\4.3.1\bin\lrelease.exe cuberok_ru.ts -qm win32\cuberok_ru.qm
 C:\Programs\Qt\4.3.1\bin\qmake.exe Cuberok.pro -o Makefile.win
 mingw32-make -f Makefile.win
