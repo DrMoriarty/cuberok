216243a483924af0ff343f2e6b9134091b8c86c3
drmoriarty0 <DrMoriarty@users.noreply.github.com>
2009-04-09 11:56:58 +0000

    patch from Issue 16

--- src/ui_cuberok.h
+++ src/ui_cuberok.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading ui file 'cuberok.ui'
 **
-** Created: Thu 9. Apr 09:43:57 2009
+** Created: Thu 9. Apr 15:46:48 2009
 **      by: Qt User Interface Compiler version 4.5.0
 **
 ** WARNING! All changes made in this file will be lost when recompiling ui file!
