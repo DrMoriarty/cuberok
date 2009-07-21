756fec955d56d704a6fd84509d2c1489cc2aa30e
drmoriarty.0 <DrMoriarty@users.noreply.github.com>
2009-07-21 13:33:17 +0000

    license fix

--- src/marklistwidget.cpp
+++ src/marklistwidget.cpp
@@ -1,12 +1,12 @@
 /* Cuberok
  * Copyright (C) 2009 Vasiliy Makarov <drmoriarty.0@gmail.com>
  *
- * This is free software; you can redistribute it and/or
+ * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
