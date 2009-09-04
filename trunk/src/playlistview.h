e6579e40f9c1d58969e82e8793f8633f67037239
drmoriarty.0 <drmoriarty.0@debfdbdd-d64f-0410-ae09-39f4d6e86eaf>
2009-09-04 06:02:58 +0000

    fix playlist view

--- trunk/src/playlistview.h
+++ trunk/src/playlistview.h
@@ -31,38 +31,22 @@
 #include "player.h"
 #include "global.h"
 
-class AbstractView : public QObject
-{
-	Q_OBJECT
- public:
-	virtual QModelIndexList getSelectedIndexes() = 0;
-	virtual QModelIndex mapToSource(QModelIndex) = 0;
-	virtual QModelIndex mapToSource(int row, int column) = 0;
-	virtual QModelIndex mapFromSource(QModelIndex) = 0;
-	virtual QModelIndex mapFromSource(int row, int column) = 0;
-	virtual void setFilterRegExp(QString s) = 0;
-	virtual void scrollTo ( const QModelIndex & index, QAbstractItemView::ScrollHint hint = QAbstractItemView::EnsureVisible ) = 0;
- public slots:
-	void clearSelection() = 0;
-	void setCurrentIndex(const QModelIndex & index) = 0;
-};
-
-class MyTreeView : public QTreeView, public AbstractView
+class MyTreeView : public QTreeView
 {
     Q_OBJECT
 public:
     MyTreeView(QString &str, PlaylistModel &model, QWidget *parent = 0);
     //~MyTreeView();
-	virtual QModelIndexList getSelectedIndexes();
+	QModelIndexList getSelectedIndexes();
 	virtual void dragEnterEvent(QDragEnterEvent *event);
     virtual void dropEvent(QDropEvent *event);
     virtual void startDrag(Qt::DropActions supportedActions);
 
-	virtual QModelIndex mapToSource(QModelIndex);
-	virtual QModelIndex mapToSource(int row, int column);
-	virtual QModelIndex mapFromSource(QModelIndex);
-	virtual QModelIndex mapFromSource(int row, int column);
-	virtual void setFilterRegExp(QString s);
+	QModelIndex mapToSource(QModelIndex);
+	QModelIndex mapToSource(int row, int column);
+	QModelIndex mapFromSource(QModelIndex);
+	QModelIndex mapFromSource(int row, int column);
+	void setFilterRegExp(QString s);
     
 protected:
 	virtual void hideEvent ( QHideEvent * event );
@@ -83,64 +67,43 @@ signals:
 	void needUpdate();
 };
 
-class PlaylistStandard : public Playlist
+
+class PlaylistAbstract : public Playlist
 {
-    Q_OBJECT
-	Q_INTERFACES(Playlist) 
-public:
-	PlaylistStandard(QString &str, QWidget *parent = 0);
-    virtual ~PlaylistStandard();
-	virtual QWidget* getWidget();
-    virtual bool isPlaying();
+	Q_OBJECT
+ public:
+	PlaylistAbstract(QString &str, QWidget *parent = 0);
+	virtual QString getName();
+	virtual void setName(QString newname);
 	virtual void storeListM3U(QString fname);
 	virtual void storeListXSPF(QString fname);
 	virtual void loadList(QString fname);
+    virtual bool isPlaying();
+	virtual void play(int index, double pos);
 	virtual void setAutosave(bool b);
-	virtual QString getName();
-	virtual void setName(QString newname);
 	virtual int curIndex();
 	virtual double curPosition();
-	virtual void play(int index, double pos);
 	virtual void rateCurrent(int offset, int value = 0);
 	virtual QString curFile();
-	virtual void setCurrent(int index);
-public slots:
+	virtual void setCurrent(int index) = 0;
+
+ public slots:
+	virtual void addUrl(QUrl url);
 	virtual void prev();
 	virtual void next();
-	virtual void play();
+	virtual void play() = 0;
 	virtual void stop();
-	virtual void clear();
-	virtual void queueNext();
-	virtual void editTag();
-	virtual void removeSong();
-	virtual void reloadTags();
-	virtual void addUrl(QUrl url);
-	virtual void setFilter(QString s);
 	virtual void findCurrent();
-private slots:
-	void onClick( const QModelIndex & index );
-	void onDoubleClick( const QModelIndex & index );
+
 	void addItem(QVariant item, int id, QModelIndex* ind = 0);
 	void playFinished();
-	void updateTag(int);
 	void position(double pos);
+	void updateTag(int);
 	void updateStatus();
-signals:
-	void status(QString);
-	void message(QString, QString, QString, long);
-	void started(Playlist*);
-	void songPosition(int);
-	void playPauseIcon (bool); // true means show a "play" icon, false means show "pause"
- private:
-    QModelIndex nextItem();
-    QModelIndex prevItem();
-	void rateSong(QModelIndex &ind, int r, int offset=0);
-    void resetTags(QModelIndex& ind);
+ private slots:
 
+ protected:
     PlaylistModel model;
-	AbstractView *view;
-	//QAbstractItemView *view;
-	//MyTreeView *view;
 	bool autosave;
     bool playing;
     QModelIndex insindex;
@@ -154,6 +117,45 @@ signals:
 	double delayedPos;
 	int error_count;
     QString info;
+
+    virtual QModelIndex nextItem() = 0;
+    virtual QModelIndex prevItem() = 0;
+    void resetTags(QModelIndex& ind);
+	void rateSong(QModelIndex &ind, int r, int offset=0);
+};
+
+
+class PlaylistStandard : public PlaylistAbstract
+{
+    Q_OBJECT
+	Q_INTERFACES(Playlist) 
+public:
+	PlaylistStandard(QString &str, QWidget *parent = 0);
+    virtual ~PlaylistStandard();
+	virtual QWidget* getWidget();
+	virtual void setCurrent(int index);
+public slots:
+	virtual void play();
+	virtual void clear();
+	virtual void queueNext();
+	virtual void editTag();
+	virtual void removeSong();
+	virtual void reloadTags();
+	virtual void setFilter(QString s);
+private slots:
+	void onClick( const QModelIndex & index );
+	void onDoubleClick( const QModelIndex & index );
+signals:
+	void status(QString);
+	void message(QString, QString, QString, long);
+	void started(Playlist*);
+	void songPosition(int);
+	void playPauseIcon (bool); // true means show a "play" icon, false means show "pause"
+ protected:
+    virtual QModelIndex nextItem();
+    virtual QModelIndex prevItem();
+
+	MyTreeView *view;
 };
 
 class PlaylistStandardFactory : public PlaylistFactory
